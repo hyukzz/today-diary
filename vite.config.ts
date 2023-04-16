80d9a95c393782bfcb5c860b0b53af0481da5840
hyukzz <ri_chard@naver.com>
2023-04-16 23:36:36 +0900

    Add: svg 사용을 위해 세팅 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { resolve } from 'path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: `${resolve()}/src` }],
   },

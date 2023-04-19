77fa6f37a1a7c2fa56c01b9212fd80684ba53f39
hyukzz <ri_chard@naver.com>
2023-04-19 23:55:31 +0900

    Feat: 모바일 앱처럼 아래에 nav 생성

---
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -0,0 +1,67 @@
+import { useNavigate } from 'react-router-dom';
+
+import Logout from '@/components/molecules/Logout/Logout';
+import { useAuthContext } from '@/components/molecules/Context/Context';
+import apple_logo from '@/assets/Logo/apple_logo.svg';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuthContext();
+
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 z-10 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="mx-auto flex items-center justify-between py-4 px-6">
+        <div className="flex items-center gap-2">
+          <img
+            src={apple_logo}
+            alt="로고"
+            className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 select-none"
+          />
+
+          <h1 className="text-md sm:text-xl md:text-2xl lg:text-2xl font-bold text-white select-none">
+            오늘의 일기
+          </h1>
+        </div>
+        {isLoggedIn && (
+          <div className="flex items-center gap-2">
+            <button
+              onClick={() => navigate('/diarywrite')}
+              className="text-sm sm:text-md md:text-lg lg:text-lg font-medium text-white focus:outline-none"
+            >
+              일기쓰기
+            </button>
+            <button
+              onClick={() => navigate('/diary')}
+              className="text-sm sm:text-md md:text-lg lg:text-lg font-medium text-white focus:outline-none mx-3"
+            >
+              일기
+            </button>
+          </div>
+        )}
+        <div className="flex items-center gap-4">
+          {isLoggedIn && <Logout />}
+          {!isLoggedIn && (
+            <>
+              <button
+                onClick={() => navigate('/login')}
+                className="text-lg font-medium text-white focus:outline-none select-none"
+              >
+                로그인
+              </button>
+              <a
+                href="https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp"
+                className="text-blue-500 font-semibold select-none"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                회원 가입
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
